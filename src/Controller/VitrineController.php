c508e9dbcb7246dec62ab026c516cd1191cadd4d
Michael <michael.polii@hotmail.com>
2019-06-05 16:25:15 +0200

    Ajout commentaire

--- src/Controller/VitrineController.php
+++ src/Controller/VitrineController.php
@@ -7,7 +7,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-
 use App\Entity\User;
 use App\Form\UserSubscribeType;
 use App\Entity\Message;
@@ -71,62 +70,24 @@ class VitrineController extends AbstractController
      */
     public function contact(Request  $request): Response
     {
-        //  $request CONTIENT LES INFORMATIONS PROVENANT DU NAVIGATEUR
-        // ( $_GET,  $_POST et  $_REQUEST)
-
-        // CET OBJET VA SERVIR A RECEVOIR LES INFOS DU FORMULAIRE
         $message = new Message();
-
-        // ENSUITE, ON CREE LE FORMULAIRE 
-        // ET ON ASSOCIE L'OBJET ENTITY AVEC LE FORMULAIRE
-        // ON CREE LE FORMULAIRE AVEC ContactType 
-        // QUI FOURNIT LES CHAMPS DU FORMULAIRE
-        // ET  $contact QUI EST L'OBJET QUI VA RECEVOIR LES INFOS DU FORMULAIRE
         $form =  $this->createForm(MessageType::class,  $message);
-
-        dump($request);
-
-        // $request->set("contact[dateMessage]", date"");
-        // ASSOCIER LE FORMULAIRE A LA REQUETE 
-        //  $request FOURNIT LES INFOS DU FORMULAIRE
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        if ($form->isSubmitted() &&  $form->isValid()) {
+
+            //CODE POUR GERER LA DATE
             $message->setDateMessage(new \DateTime);
 
-            // EST-CE QUE LE FORMULAIRE A ETE ENVOYE ?
-            if ($form->isValid()) {
-                // SECURITE: EST-CE QUE LES INFOS SONT VALIDES
-                // VERIFICATION DES CONTRAINTES Assert
+            $entityManager =  $this->getDoctrine()->getManager();
+            $entityManager->persist($message);
+            $entityManager->flush();
 
-                // OK ON A RECU LES INFOS DU FORMULAIRE
-                // ET ELLES SONT CORRECTES
-                // IL FAUT STOCKER LES INFOS EN BASE DE DONNEES
-                // => PERSISTENCE D'OBJET
-                //  $contact CONTIENT LES INFOS DU FORMULAIRE
-                // ON VA DEMANDER A PERSISTER  $contact
-
-                // ON COMPLETE LES INFOS MANQUANTES
-                // dateMessage
-                // https://www.php.net/manual/fr/class.datetime.php
-
-                // ET ENSUITE ON DEMANDE LA PERSISTANCE DES INFOS
-                $entityManager =  $this->getDoctrine()->getManager();
-                $entityManager->persist($message);  // PREPARE
-
-                $entityManager->flush();            // EXECUTE: LANCE LA REQUETE SQL
-
-                // OK TOUT S'EST BIEN PASSE
-                $messageFeedback = "MERCI POUR VOTRE MESSAGE";
-
-                // JE NE VEUX PAS CHANGER DE PAGE
-                // return  $this->redirectToRoute('contact_index');
-            }
+            //CODE POUR GERER LE FEEDBACK
+            $messageFeedback = "MERCI POUR VOTRE MESSAGE";
         }
 
         return  $this->render('vitrine/contact.html.twig', [
-            // ICI IL FAUT RAJOUTER TOUTES LES VARIABLES 
-            // QUI SERONT TRANSMISES A TWIG
             'messageFeedback'   =>  $messageFeedback ?? "",
             'contact'           =>  $message,
             'form'              =>  $form->createView(),
@@ -148,6 +109,7 @@ class VitrineController extends AbstractController
             //CODE POUR GERER LA DATE
             $newUser->setDateInscription(new \DateTime);
 
+            //CODE POUR ATTRIBUER UN ROLE A L'UTILISATEUR
             $newUser->setRole("ROLE_MEMBRE");
 
             //CODE POUR GERER LE HASHAGE DU PASSWORD
