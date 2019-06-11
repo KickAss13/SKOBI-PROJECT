b1f1b63036ae7364db787acdf7c2ac144368fab2
Michael <michael.polii@hotmail.com>
2019-06-11 12:27:21 +0200

    Gestion page evenement

--- src/Migrations/Version20190611093607.php
+++ src/Migrations/Version20190611093607.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190530141549 extends AbstractMigration
+final class Version20190611093607 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20190530141549 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE fournisseur (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(160) NOT NULL, content LONGTEXT NOT NULL, image_src VARCHAR(255) NOT NULL, date_publication DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('DROP TABLE sauce');
+        $this->addSql('ALTER TABLE event ADD category VARCHAR(160) NOT NULL, ADD date_event DATETIME NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +31,7 @@ final class Version20190530141549 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE fournisseur');
+        $this->addSql('CREATE TABLE sauce (id INT AUTO_INCREMENT NOT NULL, nom_sauce VARCHAR(160) NOT NULL COLLATE utf8mb4_unicode_ci, description LONGTEXT NOT NULL COLLATE utf8mb4_unicode_ci, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE event DROP category, DROP date_event');
     }
 }
