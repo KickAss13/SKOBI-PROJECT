40d33be8283d416856891c98e1d4245382941c5c
Michael <michael.polii@hotmail.com>
2019-06-13 15:52:29 +0200

    Maj responsive

--- public/assets/js/script-nav.js
+++ public/assets/js/script-nav.js
@@ -15,7 +15,7 @@ $(function () {
 }); */
 
 
-/*
+
 $('.menu-scroll').hide();
 
 $(window).scroll(function () {
@@ -28,5 +28,5 @@ $(window).scroll(function () {
         $('.menu-scroll').slideUp('slow');
     }
 });
-*/
+
 
