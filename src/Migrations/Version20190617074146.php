dc10dcd557acef767ef1b25e9c39112970d4b540
Michael <michael.polii@hotmail.com>
2019-06-17 15:29:29 +0200

    Maj Michael

--- src/Migrations/Version20190617074146.php
+++ src/Migrations/Version20190617074146.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190616192557 extends AbstractMigration
+final class Version20190617074146 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,6 +23,7 @@ final class Version20190616192557 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE vitamine (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(100) NOT NULL, image_src VARCHAR(160) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE article ADD slogan VARCHAR(255) DEFAULT NULL, ADD ingredients LONGTEXT DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -31,5 +32,6 @@ final class Version20190616192557 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE vitamine');
+        $this->addSql('ALTER TABLE article DROP slogan, DROP ingredients');
     }
 }
